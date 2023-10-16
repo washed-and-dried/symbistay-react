01694a3c94b093006dec8a3145ac1d9264528004
Arsh Gill <arshh.gill@outlook.com>
2023-10-16 15:32:42 +0530

    header css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header.jsx";
 import Routing from "./components/Routing.jsx";
+import Home from "./components/Home.jsx";
 
 export default function App(){
     return (
         <>
+            <Home/>
             <Header/>
-            <Routing />
+            <Routing/>
+
         </>
     )
 }
--- src/components/Routing.jsx
+++ src/components/Routing.jsx
@@ -14,5 +14,6 @@ export default function Routing(){
             <Route element={<Contact></Contact>} path={"/contact"}/>
             <Route element={<Profile></Profile>} path={"/profile"}/>
         </Routes>
+
     )
 }
