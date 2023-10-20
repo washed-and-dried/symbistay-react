3c27c11c3f1eef280f77e695769112379e3912e5
bharatm29 <bharatmhes@gmail.com>
2023-10-20 21:30:19 +0530

    footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import Header from "./components/Header.jsx";
 import Routing from "./components/Routing.jsx";
-import Home from "./components/Home.jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function App(){
     return (
         <>
-            {/*<Home/>*/}
             <Header/>
             <Routing/>
-
+            <Footer />
         </>
     )
 }
