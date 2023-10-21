5157e54e740b1365639939cc9f1e597f26c9f684
soham1011 <sohammarothi.10@gmail.com>
2023-10-21 10:42:14 +0530

    footer, mid section done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,19 @@
 import {useNavigate} from "react-router-dom";
+import Footer from "./constants/Footer";
+import MiddleSection from "./firstpageshit/Middlesection.jsx";
+
 
 export default function Home() {
     const navigate = useNavigate();
 
     return (
         <>
-            <div>This is home</div>
-            <button onClick={() => {navigate("/search", {state: {
-                    citySelectedAtHome: "Berlin" //FIXME: Prolly setup a state for the selection and other stuffs
-                }})}}>Go to search</button>
+            {/* <div>This is home</div>
+            <button onClick={() => {navigate("/search")}}>Click me!!</button> */}
+            <MiddleSection></MiddleSection>
+
+            <Footer></Footer>
         </>
+        
     );
 }
