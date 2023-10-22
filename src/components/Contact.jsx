5e15a607eab95751b989eb703f4037f740a456dd
soham1011 <sohammarothi.10@gmail.com>
2023-10-22 21:01:08 +0530

    made the profile and contact page css kinda fucked and also the git svg aint loading idek why

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,36 @@
-export default function Contact(){
+import "../styles/contact.css"
+
+export default function Contact() {
     return (
-        <h1>Contact page!</h1>
+        <>
+            <div class="contact-container">
+                <form method="get">
+                    <table border="0" cellspacing="5em">
+                        <tr>
+                            <td><label for="firstname">First Name</label></td>
+                            <td><input type="text" name="firstname" id="firstname" ></input></td>
+                            <td><label for="lastname">Last Name</label></td>
+                            <td><input type="text" name="lastname" id="lastname"></input></td>
+                        </tr>
+                        <tr>
+                            <td><label for="email">Email Address</label></td>
+                            <td colspan="2"><input type="email" name="email" id="email"></input></td>
+
+                        </tr>
+                        <tr>
+                            <td><label for="message">Message</label></td>
+                            <td colspan="3" rowspan="2"><textarea name="message" id="message" cols="3" rows="3"></textarea></td>
+                        </tr>
+                        <tr></tr>
+                        <tr>
+                            <td></td>
+                            <td><button type="button" id="contact-submit">Submit</button></td>
+                        </tr>
+                    </table>
+                </form>
+
+
+            </div>
+        </>
     )
 }
